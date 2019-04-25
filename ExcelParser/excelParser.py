c2f5efc0f91ea0b79d798f41681330734d2a14b2
StackWolfed <stackwolfed@protonmail.com>
2019-04-25 16:17:39 +0200

    setup parser

--- ExcelParser/excelParser.py
+++ ExcelParser/excelParser.py
@@ -1,3 +1,15 @@
 #!python
 # Uses Python 3
+# needs to install openpyxl
 
+from openpyxl import load_workbook
+
+wb = load_workbook("./parse.xlsx")
+
+print(wb.sheetnames)
+
+for sheet in wb:
+  if sheet.title == "GlossaryNotes":
+    continue
+  # Take sheet name, trim the 'n - ' (Where n is a number) and set it as character name
+  
