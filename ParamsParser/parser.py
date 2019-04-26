0771430954731c235ae188d12cd5e0483daafd23
StackWolfed <stackwolfed@protonmail.com>
2019-04-26 13:17:43 +0200

    cleaned code adding functions

--- ParamsParser/parser.py
+++ ParamsParser/parser.py
@@ -4,24 +4,31 @@ import requests
 import re
 import itertools
 
-# <tr>(.*?)<\/tr>
-weigthUrl = "http://kuroganehammer.com/Ultimate/Weight"
-response = requests.get(weigthUrl)
+def createDict(cleanList):
+  weightDict = {}
+  for clean in cleanList:
+    singleColumn = ''.join(ch for ch, _ in itertools.groupby(clean))
+    charEntry = singleColumn.split(';')
+    if (len(charEntry) > 3):
+      weightDict[charEntry[2]] = charEntry[3]
+  return weightDict
 
-entries = re.findall(r"<tr>(.*?)<\/tr>", response.text, re.MULTILINE | re.DOTALL)
+def parseResponse(responseText):
+  entries = re.findall(r"<tr>(.*?)<\/tr>", responseText, re.MULTILINE | re.DOTALL)
+  cleaned = []
+  for entry in entries:
+    cleaned.append(re.sub(r'<(.*?)>', ';', entry.replace('\t', '').replace('\n', ''), count=15, flags=(re.MULTILINE | re.DOTALL)))
+  cleaned.pop(0)
+  cleaned.pop(0)
+  return cleaned
 
-cleaned = []
-for entry in entries:
-  cleaned.append(re.sub(r'<(.*?)>', ';', entry.replace('\t', '').replace('\n', ''), count=15, flags=(re.MULTILINE | re.DOTALL)))
+if __name__ == "__main__":
+  weigthUrl = "http://kuroganehammer.com/Ultimate/Weight"
 
-cleaned.pop(0)
-cleaned.pop(0)
+  response = requests.get(weigthUrl)
 
-weightDict = {}
-for clean in cleaned:
-  singleColumn = ''.join(ch for ch, _ in itertools.groupby(clean))
-  charEntry = singleColumn.split(';')
-  if (len(charEntry) > 3):
-    weightDict[charEntry[2]] = charEntry[3]
+  cleaned = parseResponse(response.text)
 
-print(weightDict)
+  weightDict = createDict(cleaned)
+
+  print(weightDict)
