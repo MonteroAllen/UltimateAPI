e5b540029c0a506ba0bda72c563b59cc73091ae5
StackWolfed <stackwolfed@protonmail.com>
2019-04-26 12:39:10 +0200

    better parsing, still WIP

--- ParamsParser/parser.py
+++ ParamsParser/parser.py
@@ -11,6 +11,6 @@ entries = re.findall(r"<tr>(.*?)<\/tr>", response.text, re.MULTILINE | re.DOTALL
 
 cleaned = []
 for entry in entries:
-  cleaned.append(re.sub(r'<(.*?)>', '', entry.replace('\t', ';').replace('\n', ''), count=15, flags=(re.MULTILINE | re.DOTALL)))
+  cleaned.append(re.sub(r'<(.*?)>', ';', entry.replace('\t', '').replace('\n', ''), count=15, flags=(re.MULTILINE | re.DOTALL)))
 
 print(cleaned)
