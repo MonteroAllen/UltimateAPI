fdb94526ab6f5322d34ede1717a469b72f2383a1
StackWolfed <stackwolfed@protonmail.com>
2019-04-26 12:35:30 +0200

    Kurogane stats parser

---
+++ ParamsParser/parser.py
@@ -0,0 +1,16 @@
+#!python
+
+import requests
+import re
+
+# <tr>(.*?)<\/tr>
+url = "http://kuroganehammer.com/Ultimate/Weight"
+response = requests.get(url)
+
+entries = re.findall(r"<tr>(.*?)<\/tr>", response.text, re.MULTILINE | re.DOTALL)
+
+cleaned = []
+for entry in entries:
+  cleaned.append(re.sub(r'<(.*?)>', '', entry.replace('\t', ';').replace('\n', ''), count=15, flags=(re.MULTILINE | re.DOTALL)))
+
+print(cleaned)
