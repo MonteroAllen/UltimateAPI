156d1a5044b9e7f767bc5440bcc28aa7fbed8b36
StackWolfed <stackwolfed@protonmail.com>
2019-04-26 13:10:49 +0200

    Getting Weights

--- ParamsParser/parser.py
+++ ParamsParser/parser.py
@@ -2,10 +2,11 @@
 
 import requests
 import re
+import itertools
 
 # <tr>(.*?)<\/tr>
-url = "http://kuroganehammer.com/Ultimate/Weight"
-response = requests.get(url)
+weigthUrl = "http://kuroganehammer.com/Ultimate/Weight"
+response = requests.get(weigthUrl)
 
 entries = re.findall(r"<tr>(.*?)<\/tr>", response.text, re.MULTILINE | re.DOTALL)
 
@@ -13,4 +14,14 @@ cleaned = []
 for entry in entries:
   cleaned.append(re.sub(r'<(.*?)>', ';', entry.replace('\t', '').replace('\n', ''), count=15, flags=(re.MULTILINE | re.DOTALL)))
 
-print(cleaned)
+cleaned.pop(0)
+cleaned.pop(0)
+
+weightDict = {}
+for clean in cleaned:
+  singleColumn = ''.join(ch for ch, _ in itertools.groupby(clean))
+  charEntry = singleColumn.split(';')
+  if (len(charEntry) > 3):
+    weightDict[charEntry[2]] = charEntry[3]
+
+print(weightDict)
