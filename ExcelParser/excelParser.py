0d030e07d1e6bd1b4d4ce084101d045bc88fe4f4
StackWolfed <stackwolfed@protonmail.com>
2019-05-17 17:20:08 +0200

    started excel parser

--- ExcelParser/excelParser.py
+++ ExcelParser/excelParser.py
@@ -3,6 +3,7 @@
 # needs to install openpyxl
 
 from openpyxl import load_workbook
+import re
 
 wb = load_workbook("./parse.xlsx")
 
@@ -12,4 +13,13 @@ for sheet in wb:
   if sheet.title == "GlossaryNotes":
     continue
   # Take sheet name, trim the 'n - ' (Where n is a number) and set it as character name
-  
+  print("Splitting: " + sheet.title)
+  charName = re.split(r"[a-zA-Z]+", sheet.title)[0]
+  # From each line I have: 
+  # Column | Desc
+  # 0 move name
+  # 1 Startup, 2 Total Frames, Landing lag
+  # 5 Base dmg, 6 shieldlag, 7 shieldstun, 8 which hitbox, 9 Advantage
+  for line in sheet:
+    if line[0].value != None:
+      print(line[0].value)
